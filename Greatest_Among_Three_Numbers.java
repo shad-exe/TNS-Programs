52f8866af45d5c04738ec3400713b33eb328d6ba
Md Shad Imam <136675809+shad-exe@users.noreply.github.com>
2023-12-14 14:41:18 +0530

    Add files via upload

---
+++ Greatest_Among_Three_Numbers.java
@@ -0,0 +1,23 @@
+
+public class Greatest_Among_Three_Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 100;
+		int b = 150;
+		int c = 200;
+
+		if(a>b && a>c)
+		{
+			System.out.println(a+" is the Greatest");
+		}
+		else if (b>a && b>c)
+		{
+			System.out.println(b+" is the Greatest");
+		}
+		else
+		{
+			System.out.println(c+" is the Greatest");
+		}
+			}
+		}
